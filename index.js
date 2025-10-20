a01e69363b098e326dc101776aaed595aea9f052
Shakawatullah-Tanin <mdshakawatullahtanin23@gmail.com>
2025-10-20 16:52:52 +0600

    connect mongodb

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express")
 
+const {MongoClient,ServerApiVersion}=require('mongodb');
 const app = express()
 
 const port = process.env.PORT || 5006
@@ -7,6 +8,45 @@ const port = process.env.PORT || 5006
 
 // tanin
 // password fgWeVv1XfTHHy1Iv
+
+
+
+const uri = "mongodb+srv://tanin:fgWeVv1XfTHHy1Iv@cluster0.vivw2nj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+
+async function run() {
+
+    try{
+
+        //make client connect 
+
+        await client.connect() 
+
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+    }
+    catch{
+
+    }
+
+    finally{
+        
+
+    }
+    
+}
+
+run().catch(console.dir)
+
 app.get("/",(req,res)=>{
 
     res.send("Hello Crud")
